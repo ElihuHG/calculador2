2d1910e3466edea095ecd880e3aa224ea4a9f902
ElihuHG <madafakanew@hotmail.com>
2021-10-21 15:26:16 -0500

    aa

--- js/mainC.js
+++ js/mainC.js
@@ -3,10 +3,11 @@
 
 $(document).ready(function(){
     $("#btnCalcular").click(function(){
-       var operador = $("#operador").val();
+       var operador = $("#res1").val();
        var v1 = parseInt( $("#primNum").val());
        var v2 = parseInt( $("#segNum").val());
        var res=''; 
+        
 
 
 
@@ -28,6 +29,10 @@ $(document).ready(function(){
         res = v1/v2;
        
          }  
+
+         else{
+           alert("Ese no es un operador")
+         }
 
          $("#resultado").text("el resultado es " + res);
 
